cb82cd5d8d1521b76561679c4065c1d4e9f430e6
QABlu <alirezajam98@gmail.com>
2023-06-17 11:27:03 +0330

    tamrin 1 ,2 ,3

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+//-----------------------------زوج و فرد---------------------------------
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("please enter number for check zoj or fard : ");
+//        int number = scanner.nextInt();
+//        if (number % 2 == 0)
+//            System.out.println("zoj ast.");
+//        else
+//            System.out.println("fard ast.");
+//    }
+//}
+
+//_________________________________نمره 0تا 20 ________________________________________
+
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("please enter Nomre : ");
+//        int nomre = scanner.nextInt();
+//        if (nomre < 0 || nomre > 20) {
+//            System.out.println("adade vared shode sahih nemibashad. lodfan dart baze 0 ta 20 vared konid");
+//        } else if (nomre < 10) {
+//            System.out.println("mashroot");
+//        } else if (nomre < 15) {
+//            System.out.println("bad");
+//        } else if (nomre < 18) {
+//            System.out.println("khob");
+//        } else {
+//            System.out.println("Aali");
+//        }
+//
+//    }
+//}
+
+//____________________________________________جدول ضرب ________________________________________________________
+
+//public class Main {
+//    public static void main(String[] args) {
+//        for (int x = 1; x <= 10; x++) {
+//            for (int y = 1; y <= 10; y++) {
+//                System.out.print(x * y + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
