50a6e5c412e5f3c5888662675d6f3e9912094b78
javad <javad.aboutorabi@gmail.com>
2023-06-20 21:58:51 +0330

    feat: first commit javad

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,69 @@
-//-----------------------------زوج و فرد---------------------------------
-//import java.util.Scanner;
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("please enter number for check zoj or fard : ");
-//        int number = scanner.nextInt();
-//        if (number % 2 == 0)
-//            System.out.println("zoj ast.");
-//        else
-//            System.out.println("fard ast.");
-//    }
-//}
 
-//_________________________________نمره 0تا 20 ________________________________________
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("please enter Nomre : ");
-//        int nomre = scanner.nextInt();
-//        if (nomre < 0 || nomre > 20) {
-//            System.out.println("adade vared shode sahih nemibashad. lodfan dart baze 0 ta 20 vared konid");
-//        } else if (nomre < 10) {
-//            System.out.println("mashroot");
-//        } else if (nomre < 15) {
-//            System.out.println("bad");
-//        } else if (nomre < 18) {
-//            System.out.println("khob");
-//        } else {
-//            System.out.println("Aali");
-//        }
-//
-//    }
-//}
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        if (a == 0) {
+            evenAndOdd();
+        } else if (a == 1) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("please enter a score : ");
+            score(scanner.nextInt());
+        } else if (a == 2) {
+            table();
+        }else if (a==3){
+            maxNumber();
+        }
 
-//____________________________________________جدول ضرب ________________________________________________________
 
-//public class Main {
-//    public static void main(String[] args) {
-//        for (int x = 1; x <= 10; x++) {
-//            for (int y = 1; y <= 10; y++) {
-//                System.out.print(x * y + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
-//}
+    }
+
+    private static void maxNumber() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0 ; i<20;i++){
+            int r = random.nextInt(100);
+            numbers.add(r);
+        }
+        System.out.println(numbers);
+
+    }
+
+    public static void table() {
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 1; y <= 10; y++) {
+                System.out.print(x * y + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void score(int score) {
+        if (score < 0 || score > 20) {
+            System.out.println("adade vared shode sahih nemibashad. lodfan dart baze 0 ta 20 vared konid");
+        } else if (score < 10) {
+            System.out.println("mashroot");
+        } else if (score < 15) {
+            System.out.println("bad");
+        } else if (score < 18) {
+            System.out.println("khob");
+        } else {
+            System.out.println("Aali");
+        }
+    }
+
+    public static void evenAndOdd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter number for check Even or Odd : ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0)
+            System.out.println(number + " is Even");
+        else
+            System.out.println(number + " is Odd");
+    }
+
+
+}
